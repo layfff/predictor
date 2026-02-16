4aa1da4507292edfa3f4d81f0b8db38e5812726c
layfff <sevnickijsergej370@gmail.com>
2026-02-16 16:47:06 +0500

    deploy app
    index.html, and other files

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+const answersArray = [
+  "Стоит",
+  "Обязательно сделай это",
+  "Это очень плохо",
+  "Точно нет",
+  "Скорее всего можно",
+  "Можно сделать, но если аккуратно",
+  "За такое можно и по шапке получить",
+]
+const answerElement = document.querySelector("[data-js-answer]")
+const fieldElement = document.querySelector("[data-js-field]")
+console.log(answerElement)
+
+function getAnswer(answerArray) {
+  const randomIndex = Math.floor(Math.random() * answerArray.length)
+  return answerArray[randomIndex]
+}
+
+function showAnswer() {
+  if (fieldElement.value.trim() === '') {
+    return answerElement.textContent = "Вы не ввели запрос."
+  }
+
+  const answer = getAnswer(answersArray)
+  answerElement.textContent = answer
+}
+
+document.getElementById('form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Останавливает перезагрузку
+});
